3dbce40abe20ac3d2db86af57562c2770652b6db
mydreamis-a <9104141@gamil.com>
2022-07-18 16:08:50 +0900

    새로운 시작, 엉망진창이어도 괜찮아

---
+++ 07.18/2_index.js
@@ -0,0 +1,26 @@
+// npm 설치 관련
+// 메타 데이터 파일을 가지고 있는 json 초기화 명령어
+// 메타 데이터는 데이터들을 설명해주는 데이터 (속성)
+// npm init -y
+
+// ctrl + c: 터미널 취소 명령어
+
+// {
+//     "name": 패키지 이름,
+//     "version": 버전,
+//     "description": 설명,
+//     "main": 메인의 파일,
+//     "scripts": {
+//       "실행 명령어": "",
+//       "strat": "node index.js",
+//       "test": "node index2.js"
+//     },
+//     "keywords": 검색 키워드,
+//     "author": 작업자 정보,
+//     "license": 모듈의 라이센스를 기록
+// }
+  
+// scripts 실행
+// npm run test
+// 예외: npm start
+// cd 경로와 json 및 js 파일 경로 확인 필수
