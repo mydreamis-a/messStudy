e15ea5c348b9df77e85a993e825b59311bc170db
mydreamis-a <9104141@gmail.com>
2022-07-21 19:37:18 +0900

    07 21 수업 추가

--- 07.19/src/2_main.js
+++ 07.19/src/2_main.js
@@ -1,3 +1,5 @@
+// 07 20 수
+
 // MYSQL 연결하기
 
 // MYSQL은 콜백 기반으로 promise를 사용을 위해서는 별도의 promise-mysql의 설치가 필요한데
@@ -22,13 +24,17 @@ const temp = mysql.createConnection({
   database: "test0719",
 });
 
+// 07 21 목
+
 // ㅜ query 함수의 첫 번째 매개 변수: 쿼리문
 // ㅜ 두 번째 매개 변수: 쿼리 에러, 쿼리 결과, 필드 명이 담긴 콜백 함수
-temp.query("SELECT * FROM posts", (err, res) => {
+temp.query("SELECT * FROM products", (err, res) => {
   if (err) {
-    console.log("error");
+    const sql =
+      "CREATE TABLE products(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), number VARCHAR(20), series VARCHAR(20))";
+    temp.query(sql);
   } else {
-    console.log(res);
+    console.log("res: ", res);
   }
 });
 
@@ -36,8 +42,40 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   req.statusCode = 200;
-  res.write("123");
-  res.end("456");
+
+  // ㅜ 한글이 깨질 때는 인코딩 방식을 정해보자.
+  // ㅜ res setHeader(내용) 함수를 사용해서 헤더의 정보를 설정할 수 있다.
+  // ㅜ utf-8로 컨텐츠 내용의 인코딩 속성을 추가한다.
+  res.setHeader("Content-Type", "application/json", "charset=utf-8");
+
+  // req.url: 요청된 url 확인
+  // req.method: 요청된 method 확인
+
+  const URL = req.url;
+  switch (URL) {
+    case "/":
+      res.end("메인 페이지");
+      break;
+    case "/list":
+      temp.query("SELECT * FROM products", (err, data) => {
+        if (err) {
+          console.log("error: ", err);
+        } else {
+          // ㅜ data: products 테이블 안의 컬럼 내용
+          res.end(JSON.stringify(data));
+        }
+      });
+      break;
+    case "/add":
+      // ㅜ 두 번째 매개 변수로 배열 타입을 추가할 수 있다.
+      // eslint-disable-next-line no-case-declarations
+      const sql =
+        "INSERT INTO products (name, number, series) VALUES (?, ?, ?)";
+      temp.query(sql, ["Jang", "28", ""]);
+      break;
+    default:
+      break;
+  }
 });
 
 const PORT = 3000;
@@ -48,4 +86,14 @@ server.listen(PORT, () => {
 // 경로 확인: node 띄고 tap
 // 실행: node src/2_main.js
 
-// 07 19 19 수정
+// npdemon: 자동으로 모디터링 해서 js의 내용이 변경되면 서버를 재시작해주는 툴
+
+// nodemon 설치 명령어
+// npm install --save-dev nodemon
+
+// nodemon 삭제 명령어
+// npm uninstall -g nodemon
+
+// package.json 파일의 scripts: start 부분 nodemon으로 수정 후 npm start로 실행
+
+// 07 21 19 수정
